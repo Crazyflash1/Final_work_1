e62e8b4af2801bd4aefd1b4d6995a80918fb5013
Crazyflash1 <140066440+Crazyflash1@users.noreply.github.com>
2024-01-22 14:14:12 +0300

    final

--- 1-easyrsa-openvpn.sh
+++ 1-easyrsa-openvpn.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-#load variables
+#variables
 . ./variables
 var
-# устанавливаем easy-rsa и openvpn
+if [[ "$EUID" -ne 0 ]]; then
+	echo "This installer needs to be run with superuser privileges."
+	exit
+fi
+# install easy-rsa и openvpn
 apt-get update
 apt-get install easy-rsa openvpn
 mkdir -p $KEY_DIR $OUTPUT_DIR $EASY_RSA_DIR
+cp variables /opt/clients/
 ln -s /usr/share/easy-rsa/* $EASY_RSA_DIR
 cd $EASY_RSA_DIR
 ./easyrsa init-pki
-#создание файл запроса сертификата и ключ
+#create req and key 
 ./easyrsa gen-req server nopass
+cp /opt/easy-rsa/pki/private/server.key $VPN_SERVER_DIR
